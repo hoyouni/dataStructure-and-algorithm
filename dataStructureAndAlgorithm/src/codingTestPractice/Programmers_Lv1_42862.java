067c626ddd9716505429f81c1948c2a3476c2a99
hoyouni <sgm00006@gmail.com>
2021-03-23 18:26:04 +0900

    Programmers_Lv1_42862

---
+++ dataStructureAndAlgorithm/src/codingTestPractice/Programmers_Lv1_42862.java
@@ -0,0 +1,29 @@
+package codingTestPractice;
+
+public class Programmers_Lv1_42862 {
+	public static int solution(String dartResult) {
+		
+		
+		return -1;
+    }
+	
+	public static void main(String[] args) {
+		String dartResult = "1S*2T*3S";
+		int result = solution(dartResult);
+		System.out.println(result);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
